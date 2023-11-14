5400c46fa6ec722b0f6ae774e94b12155a797040
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-13 20:24:13 -0500

    added topper

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Promo from './components/Promo';
 import CoffeeList from './components/CoffeeList';
 import Footer from './components/Footer';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Promo />
       <CoffeeList />
       <Footer />
     </div>
---
+++ src/components/Promo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Promo = () => {
+  return (
+    <div className="promo">
+      <div className="image-container">
+        <img src="https://nypost.com/wp-content/uploads/sites/2/2023/01/coffee.jpg" alt="Description" />
+      </div>
+      <div className="text-container">
+        <p>Understanding coffee roasts is crucial for coffee enthusiasts and connoisseurs as it significantly influences the flavor, aroma, and overall profile of the brewed coffee. The roasting process transforms green coffee beans into the aromatic, flavorful beans we use for brewing. Different roast levels, such as light, medium, and dark, impart distinct characteristics to the coffee, ranging from bright and fruity notes in light roasts to robust and smoky flavors in dark roasts. Knowing coffee roasts allows individuals to tailor their coffee experience based on personal preferences, providing insight into the nuanced world of coffee varieties and helping them appreciate the diverse range of flavors that can be derived from the same humble coffee bean.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Promo;
