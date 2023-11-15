2ce68d227244b38e76bb04e32e92c91a574eb285
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 17:36:51 -0500

    going back to original styling

--- src/components/CoffeeItem.jsx
+++ src/components/CoffeeItem.jsx
@@ -1,10 +1,10 @@
 const CoffeeItem = ({ coffee }) => {
- 
+
   return (
     <div className="coffee-item">
       <h3>{coffee.name}</h3>
       <p>Origin: {coffee.origin}</p>
-      <p>Roast Level: {coffee.roastLevel}</p>
+     <p>Roast Level: {coffee.roastLevel}</p>
       <p>Flavor Profile: {coffee.flavorProfile}</p>
     </div>
   );
