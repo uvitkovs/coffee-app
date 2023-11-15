c16077f21de4cfc2c6e08660c6a23fcd4392d2d5
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 18:24:00 -0500

    added coffee roasts image and text

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -9,6 +9,12 @@ const Promo = () => {
       <div className="text-container">
         <p>Understanding coffee roasts is crucial for coffee enthusiasts and connoisseurs as it significantly influences the flavor, aroma, and overall profile of the brewed coffee. The roasting process transforms green coffee beans into the aromatic, flavorful beans we use for brewing. Different roast levels, such as light, medium, and dark, impart distinct characteristics to the coffee, ranging from bright and fruity notes in light roasts to robust and smoky flavors in dark roasts. </p> 
         <p>Knowing coffee roasts allows individuals to tailor their coffee experience based on personal preferences, providing insight into the nuanced world of coffee varieties and helping them appreciate the diverse range of flavors that can be derived from the same humble coffee bean.</p>
+      <p>Now, let's explore the range of possibilities:</p>
+      </div>
+      <div>
+      <div className="image-container">
+        <img src="https://coffeeaffection.com/wp-content/uploads/2019/10/The-4-Types-of-Coffee-Roasts.png" alt="coffee roasts" />
+      </div>
       </div>
     </div>
   );
