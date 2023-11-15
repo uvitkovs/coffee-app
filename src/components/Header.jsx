70a8cdde45ae06318b71b4910c1b7a9c8ec1b2a2
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 16:39:13 -0500

    added navigation in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,14 @@ const Header = () => {
   return (
     <header className="App-header">
       <h1>Coffee Roasts for Everyone</h1>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
