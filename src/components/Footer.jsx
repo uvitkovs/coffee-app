d91fbd1640d2541ec427a904e53729bc967ce225
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 17:54:30 -0500

    added navigation links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,14 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="App-footer">
+         <nav>
+        <ul className="footer-links">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
       <p>2023 Coffee Roasts by Julie Vitkovskaya</p>
     </footer>
   );
