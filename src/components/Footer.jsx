9057834864fc356ffa897ac42490b72f675b16be
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 17:56:12 -0500

    changed text of footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
          <nav>
         <ul className="footer-links">
           <li><a href="/home">Home</a></li>
-          <li><a href="/about">About</a></li>
+          <li><a href="/about">Our Story</a></li>
           <li><a href="/products">Products</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
