bdc7a452a409a64d1d3b276b6d8c8c3ce71672f8
Julie Vitkovskaya <uvitkovs@gmail.com>
2023-11-15 17:48:55 -0500

    changing text for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="App-header">
-      <h1>Coffee Roasts for Everyone</h1>
+      <h1>Which coffee roast is right for me?</h1>
       <nav>
         <ul className="nav-links">
           <li><a href="/home">Home</a></li>
-          <li><a href="/about">About</a></li>
+          <li><a href="/about">Our Story</a></li>
           <li><a href="/products">Products</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
